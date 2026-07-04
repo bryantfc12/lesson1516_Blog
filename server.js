ddb21658eaf72ecdd3580b5a9975cb42942f9f49
bryantfc12 <bryantfchen1@gmail.com>
2026-07-04 14:04:12 +0700

    new push

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import {engine} from 'express-handlebars'
+import session from 'express-session'
+import { trusted } from 'mongoose'
+
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+app.use(express.static('public'))
+
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.render('index', {
+        title : "Homepage",
+        username : 'Bryant'
+    })
+})
+
+app.get("/about", (req,res)=>{
+    res.render('about', {
+        title : "About",
+        message : "Welcome to my about page and read all of my bios!"
+    })
+})
+
+const port = 3006
+app.listen(port, ()=>{
+    console.log(`Server running on localhost:${port}`)
+})
